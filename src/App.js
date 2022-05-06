c2996bf4301d97f011388ef753490583a9a1f88a
jatomic <johnatomich@gmail.com>
2022-05-06 12:50:33 -0500

    Custom text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Here is my custom code!!!
         </p>
         <a
           className="App-link"
